4dcb9a69beaf78015b39756fdb9b92d12ebb8a6e
eleadn <eleano.dn@gmail.com>
2025-04-17 22:29:04 +0200

    05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (!Number.isInteger(num1) || !Number.isInteger(num2) ||
+        num1 < 0 || num2 < 0)
+    {
+        return "ERROR";
+    }
+    
+    let min = num1 <= num2 ? num1 : num2;
+    let max = num1 > num2 ? num1 : num2;
+    let result = 0;
 
+    for (let i = min; i <= max; ++i)
+    {
+        result += i;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
