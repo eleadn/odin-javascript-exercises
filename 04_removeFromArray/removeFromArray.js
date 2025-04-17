625d5dc98afdef01cfa2f6a6e47e546f2b952a1c
eleadn <eleano.dn@gmail.com>
2025-04-17 22:20:55 +0200

    04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    let result = [];
+
+    for (let i = 0; i < arr.length; ++i)
+    {
+        if (!args.find(x => x === arr[i]))
+        {
+            result.push(arr[i]);
+        }
+    }
+
+    return result;
 };
 
 // Do not edit below this line
