29205b1dd2f4d71dc946a3c27f59ad92d1b74591
eleadn <eleano.dn@gmail.com>
2025-04-20 12:30:33 +0200

    12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.sort(
+        (a, b) =>
+        {
+            let aYearOfDeath = "yearOfDeath" in a ? a.yearOfDeath : new Date().getFullYear();
+            let bYearOfDeath = "yearOfDeath" in b ? b.yearOfDeath : new Date().getFullYear();
+            return aYearOfDeath - a.yearOfBirth > bYearOfDeath - b.yearOfBirth ? -1 : 1;
+        }
+    ).at(0);
 };
 
 // Do not edit below this line
