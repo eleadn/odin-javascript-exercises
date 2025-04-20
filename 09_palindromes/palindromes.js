54e8adf34355a2926c334158ae1935b3cfb63335
eleadn <eleano.dn@gmail.com>
2025-04-20 12:07:15 +0200

    09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const authorizedChar = "abcdefghijklmnopqrstuvwxyz0123456789";
 
+    const pureStr = str
+        .toLowerCase()
+        .split('')
+        .filter(char => authorizedChar.includes(char))
+        .join('');
+    return pureStr === pureStr.split('').reverse().join('');
 };
 
 // Do not edit below this line
