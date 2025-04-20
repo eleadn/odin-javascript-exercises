fbb9770736e7ceb8489cdff36ded086f9a954a54
eleadn <eleano.dn@gmail.com>
2025-04-20 12:12:32 +0200

    10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    if (index < 0)
+    {
+        return "OOPS";
+    }
 
+    let last = 1;
+    let current = 0;
+
+    for (let i = 0; i < index; ++i)
+    {
+        const cur = current;
+        current += last;
+        last = cur;
+    }
+
+    return current;
 };
 
 // Do not edit below this line
