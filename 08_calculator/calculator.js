820beb318b666629ade7d570d4c707bb075356f4
eleadn <eleano.dn@gmail.com>
2025-04-20 11:52:33 +0200

    08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	return numbers.length === 0 ? 0 : numbers.reduce((total, number) => total + number);
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  return numbers.length === 0 ? 0 : numbers.reduce((total, number) => total * number);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	let result = 1;
+  while (number > 1)
+  {
+    result *= number;
+    --number;
+  }
+  return result;
 };
 
 // Do not edit below this line
